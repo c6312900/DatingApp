247ddb328b8702536a806789f0af7e4c4d15f752
Bill Fu <c6312900@gmail.com>
2020-02-07 09:08:09 +0800

    End Of Session 8

--- DatingApp.API/Helpers/Extensions.cs
+++ DatingApp.API/Helpers/Extensions.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Http;
 
 namespace DatingApp.API.Helpers
@@ -9,6 +10,15 @@ namespace DatingApp.API.Helpers
             response.Headers.Add("Application-Error", message);
             response.Headers.Add("Access-Control-Expose-Headers","Application-Error");
             response.Headers.Add("Access-Control-Allow-Origin","*");
+        }
+
+        public static int CaculateAge(this DateTime thisDatetime)
+        {
+            var age = DateTime.Today.Year - thisDatetime.Year;
+            if (thisDatetime.AddYears(age) > DateTime.Today)
+               age--;
+            return age;
+            
         }
         
     }
